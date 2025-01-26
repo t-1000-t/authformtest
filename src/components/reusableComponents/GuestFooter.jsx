4e6658c8c261406da6b2c43875d05479e5b33e9b
vl <goognya@gmail.com>
2025-01-26 19:11:50 +0000

    Was add file for styles and GuestFooter.jsx padding X with value 3

--- src/components/reusableComponents/GuestFooter.jsx
+++ src/components/reusableComponents/GuestFooter.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Stack, Text } from '@chakra-ui/react'
 import Container from './Container'
+import './reusableStyle.css'
 
 const GuestFooter = () => {
   return (
-    <Container as={Stack} py={4} bg="teal.500" maxW="1440px" rounded={{ md: '2xl' }}>
-      <Stack direction={{ base: 'column', sm: 'row' }} justify="center" align="center" spacing={8} color="black">
+    <Container as={Stack} py={4} px={3} bg="teal.500" maxW="1440px" rounded={{ md: '2xl' }} className="guest-fc">
+      <Stack direction={{ base: 'column', sm: 'row' }} justify="center" align="center" spacing={8}>
         <Text _hover={{ textDecoration: 'underline' }}>
           <Link to={{ pathname: '/' }}>Home</Link>
         </Text>
